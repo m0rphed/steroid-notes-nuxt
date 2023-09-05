ccce767a10178e73e72214f68c813ef4b5ca100c
m0rphed <35064145+m0rphed@users.noreply.github.com>
2023-09-05 11:14:15 +0300

    NuxtLayout isnside div

--- app.vue
+++ app.vue
@@ -3,38 +3,40 @@ const route = useRoute();
 </script>
 
 <template>
-  <nav class="flex align-center gap-4 p-4 bg-gray-700">
-    <NuxtLink
-      to="/"
-      class="
-        text-gray-400
-        n-link-base
-        cursor-pointer
-        hover:text-green-400
-        transition-colors
-        duration-300
-        ease-in-out
-      "
-    >
-      Home
-    </NuxtLink>
-    <NuxtLink
-      to="/upload"
-      class="
-        text-gray-400
-        n-link-base
-        cursor-pointer
-        hover:text-green-400
-        transition-colors
-        duration-300
-        ease-in-out
-      "
-    >
-      Upload
-    </NuxtLink>
-  </nav>
-  <!-- Markup shared across all pages, here (like NavBar) -->
   <div>
-    <NuxtPage />
+    <NuxtLayout>
+      <nav class="flex align-center gap-4 p-4 bg-gray-700">
+        <NuxtLink
+          to="/"
+          class="
+            text-gray-400
+            n-link-base
+            cursor-pointer
+            hover:text-green-400
+            transition-colors
+            duration-300
+            ease-in-out
+          "
+        >
+          Home
+        </NuxtLink>
+        <NuxtLink
+          to="/upload"
+          class="
+            text-gray-400
+            n-link-base
+            cursor-pointer
+            hover:text-green-400
+            transition-colors
+            duration-300
+            ease-in-out
+          "
+        >
+          Upload
+        </NuxtLink>
+      </nav>
+
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
