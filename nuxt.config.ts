796155e7fb4332e76ddd9bc18367c6253530aa79
Vic Khov <victorkhovtko@gmail.com>
2023-09-14 17:32:39 +0300

    configuration: changed linting and formatting settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,12 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   pages: true,
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', '@nuxtjs/eslint-module'],
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
       tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
+      autoprefixer: {}
+    }
+  }
 })
