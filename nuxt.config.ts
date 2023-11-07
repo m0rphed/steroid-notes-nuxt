ef79b1f0c3a8e1061ea061db8370c6eb3d1763a9
Vic Khov <victorkhovtko@gmail.com>
2023-11-07 08:45:53 +0300

    css updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,11 @@
 export default defineNuxtConfig({
   pages: true,
   modules: ['@nuxt/ui', '@nuxtjs/eslint-module'],
-  css: ['~/assets/css/main.css'],
+  css: [
+    '~/assets/css/main.css',
+    '~/assets/css/katex.min.css',
+    '@milkdown/theme-nord/style.css'
+  ],
   postcss: {
     plugins: {
       tailwindcss: {},
